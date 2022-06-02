50c0af67148c3b706d3ad812febef009b5fe67b3
beniv_bohdan <bohdanbeniv@gmail.com>
2022-06-02 23:05:07 +0200

    Devide onto components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./styledcomponents/Header.css"
+function Header(props:any) {
+    return (
+      <header className="header">
+      <div> <img src="https://iconape.com/wp-content/png_logo_vector/react.png" alt="img" /></div>
+      </header>
+    );
+  }
+
+  export default Header;
+  
---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+function Profile() {
+    return (
+      <div className='content'>
+      <div> <img src="https://c0.wallpaperflare.com/preview/117/132/59/state-cityscape-landscape-wide.jpg" alt="" /></div>
+     <div>ava + descr</div>
+     <div>my posts
+       <div>new post</div>
+       <div>post1</div>
+       <div>post2</div>
+     </div>
+    </div>
+    );
+  }
+
+  export default Profile;
